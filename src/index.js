862ebbbbc6cc5c8fc25986ce0507c4f0aafe00e9
Assile Zeidan <assile.zeidan@hotmail.com>
2025-03-18 11:39:02 +0100

    first assignment

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+// MIDDLEWARE
+app.use(express.json())
+    app.use((req, res, next) => {
+        req.requestTime = Date.now();
+        req.arithmetical_value = 4 * 7; 
+        next();
+});
 app.get("/", (req, res) => {
-    res.send("Hello world!")
-})
+    res.send(`Request Time: ${req.requestTime} | Arithmetical Value: ${req.arithmetical_value}`);
+ });
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
